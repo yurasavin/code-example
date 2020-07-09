598f00cd3af25966dc299e40b6b5d3cb431aada3
Yura Savin <yurasavin@gmail.com>
2020-07-09 15:40:18 +0300

    add LimitViewSet tests

--- src/limits/tests/test_views.py
+++ src/limits/tests/test_views.py
@@ -24,8 +24,8 @@ class TestLimitViewSet:
 
     def test_retrieve(self, api_client):
         limit_info = LimitDateInfoFactory()
-        url = reverse('limits-detail', kwargs={'pk': limit_info.id})
 
+        url = reverse('limits-detail', kwargs={'pk': limit_info.id})
         response = api_client.get(url)
 
         assert response.status_code == status.HTTP_200_OK
@@ -52,3 +52,33 @@ class TestLimitViewSet:
         assert response.status_code == status.HTTP_200_OK
         assert response.data['date'] == limit_info_latest.date.strftime('%Y-%m-%d')
         assert response.data['limit']['id'] == limit_info_latest.limit_id
+
+    def test_filtering_by_date(self, api_client, freezer):
+        TODAY_LIMITS_COUNT = 5
+        # create today limit infos
+        LimitDateInfoFactory.create_batch(TODAY_LIMITS_COUNT)
+
+        # Create old limit infos
+        today = datetime.date.today()
+        past_date = today - datetime.timedelta(days=30)
+        freezer.move_to(past_date)
+        LimitDateInfoFactory.create_batch(TODAY_LIMITS_COUNT)
+
+        url = reverse('limits-list')
+        params = {'date': today.strftime('%Y-%m-%d')}
+        response = api_client.get(url, params)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert isinstance(response.data, list)
+        assert len(response.data) == TODAY_LIMITS_COUNT
+
+    def test_filtering_by_limit_year(self, api_client):
+        limit_info = LimitDateInfoFactory.create_batch(5)[0]
+
+        url = reverse('limits-list')
+        params = {'limit__year': limit_info.limit.year}
+        response = api_client.get(url, params)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert isinstance(response.data, list)
+        assert len(response.data) == 1
